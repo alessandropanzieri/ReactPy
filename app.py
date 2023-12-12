85e89cc52fb50acb7038d66175de7407ebbf6789
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-12-12 21:54:18 +0100

    commit

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask
 from reactpy import component, html
+from reactpy_router import route, simple
 from reactpy.backend.flask import configure
 
 @component
+def router():
+    return simple.router(
+        route("/h1", h1()),
+        route("/h2", h2()),
+        route("/h3", h3()),
+    )
+
+@component
 def h1():
-    return html.h1("Hello World!")
+    return html.h1("H1")
 
-app = Flask(__name__)
-configure(app, h1)
+@component
+def h2():
+    return html.h2("H2")
 
-if __name__ == "__main__": app.run()
+@component
+def h3():
+    return html.h3("H3")
+
+if __name__ == "__main__":
+
+    app = Flask(__name__)
+    configure(app, router)
+
+    app.run(debug = True)
